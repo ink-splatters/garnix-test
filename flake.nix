0ba3300910a98452d5216224e5e6f4f23722b226
Peter A <ink.splatters@pm.me>
2022-08-26 01:19:06 +0200

    basic flake-parts based flake and shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+
+  description = "garnix CI test";
+
+  inputs = {
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-flake.url = "github:srid/treefmt-flake";
+  };
+
+  outputs = { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      imports = [
+       	treefmt-flake.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+	      packages.default = pkgs.hello;
+	devShells.default = import ./shell.nix
+      };
+  };
+}
+
+
+
---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs,... }:
+  with pkgs; mkShell {
+    name = "tools shell";
+  buildInputs = with pkgs ;[
+	fd
+	ripgrep
+	fzf
+	du-dust
+	procs
+	bottom 
+	xh
+	jq
+   ];
+  shellHook = ''
+    export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
+  '';
+}
